590aa81ef22fa9fd60fc55538c7f50ac17d7fdd0
your-local-developer <73035294+your-local-developer@users.noreply.github.com>
2022-01-04 14:49:20 +0100

    update test server

--- test/server/main.ts
+++ test/server/main.ts
@@ -6,50 +6,62 @@ import {
   Router,
   yellow,
 } from "../test_deps.ts";
-import { Inertia } from "../../mod.ts";
+import { Inertia, InertiaState } from "../../mod.ts";
 import { Book, books } from "./db.ts";
 
 const { __dirname } = fromMeta(import.meta);
-const app = new Application();
 
-// TODO: maybe use state instead of a local context copy?
-//const app = new Application({ state: { inertia: Inertia } });
+const app = new Application<InertiaState>();
+const router = new Router<InertiaState>();
 
-const router = new Router();
+// Export inertia to use it in controller
 const inertia = new Inertia({
-  staticDir: join(__dirname, "public"),
-  checkVersionFunction: () => {
-    return Inertia.defaultCheckManifestAssetVersion(
+  checkVersionFunction: async () => {
+    return await Inertia.checkManifestAssetVersion(
       join(__dirname, "public/manifest.json"),
     );
   },
+  templateFilePath: join(__dirname, "public/template.html"),
+  devMode: true,
 });
 
 router
   .get("/", async () => {
     await inertia.render("App");
   })
-  .get("/book", async () => {
-    await inertia.render("Book", { books: Array.from(books.values()) });
+  .get("/book", async (context) => {
+    await context.state.inertia.render("Book", {
+      books: Array.from(books.values()),
+    });
   })
   .get("/book/:id", async (context) => {
-    if (context.params && books.has(context.params.id)) {
-      const book = books.get(context.params.id);
-      if (book) {
-        const payload: { book: Book } = { book: book };
-        await inertia.render("Book", payload);
-      }
-    } else {
-      // TODO: render Error
-    }
+    // test if the header still is applied to the context copy of inertia
+    context.response.headers.append("x-test", "true");
+    // if (context.params && books.has(context.params.id)) {
+    const book = books.get(context.params.id);
+    const payload: { book?: Book } = { book: book };
+    await inertia.render("Book", payload);
+    // } else {
+    // TODO: render Error
+    // }
+  })
+  .post("/book/:id", async (context) => {
+    const ping = await context.request.body().value;
+    inertia.location("https://developer.mozilla.org/")
+  })
+  .put("/book/:id", async (context) => {
+    // TODO: fix false return of 409 when version is incorrect https://inertiajs.com/the-protocol#the-page-object#asset-versioning
+    const ping = await context.request.body().value;
+    context.response.headers.append("X-Pong", ping);
+    inertia.redirect();
   });
 
-app.use(inertia.initMiddleware);
+app.use(inertia.initMiddleware());
 
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-app.use(inertia.initStatic);
+app.use(inertia.initStatic(join(__dirname, "public")));
 
 app.addEventListener("listen", ({ hostname, port, serverType }) => {
   console.log(
--- test/test_deps.ts
+++ test/test_deps.ts
@@ -1,4 +1,8 @@
-export { fromMeta } from "https://deno.land/x/dirname_deno@v0.2.0/mod.ts";
+export { fromMeta } from "https://deno.land/x/dirname_deno@v0.3.0/mod.ts";
 export { Application, Router } from "https://deno.land/x/oak@v10.1.0/mod.ts";
+export type {
+  Context,
+  RouterContext,
+} from "https://deno.land/x/oak@v10.1.0/mod.ts";
 export { join } from "https://deno.land/std@0.119.0/path/mod.ts";
 export { bold, yellow } from "https://deno.land/std@0.118.0/fmt/colors.ts";
