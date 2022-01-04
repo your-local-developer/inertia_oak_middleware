d14856241fbca91bd1468d2197e51bf9dbaf38ef
your-local-developer <73035294+your-local-developer@users.noreply.github.com>
2022-01-04 13:47:17 +0100

    Template engine support
    Add support for multiple template-engines and update dependencies

--- deps.ts
+++ deps.ts
@@ -1,11 +1,14 @@
-export { Context } from "https://deno.land/x/oak@v10.1.0/mod.ts";
-export type { Middleware } from "https://deno.land/x/oak@v10.1.0/mod.ts";
+export { Context, REDIRECT_BACK } from "https://deno.land/x/oak@v10.1.0/mod.ts";
+export type {
+  ContextSendOptions,
+  Middleware,
+} from "https://deno.land/x/oak@v10.1.0/mod.ts";
 export { Md5 } from "https://deno.land/std@0.119.0/hash/md5.ts";
 export { Status } from "https://deno.land/std@0.119.0/http/http_status.ts";
-export { renderFile } from "https://deno.land/x/mustache@v0.3.0/mod.ts";
-export { join } from "https://deno.land/std@0.119.0/path/mod.ts";
 export {
   bold,
   green,
   yellow,
-} from "https://deno.land/std@0.118.0/fmt/colors.ts";
+} from "https://deno.land/std@0.119.0/fmt/colors.ts";
+import mustache from "https://esm.sh/mustache@4.2.0";
+export { mustache };
---
+++ src/supported_template_engines.ts
@@ -0,0 +1,24 @@
+import { mustache } from "../deps.ts";
+
+const decoder = new TextDecoder();
+
+enum SupportedTemplateEngine {
+  Mustache = "MUSTACHE",
+  Custom = "CUSTOM",
+}
+
+async function renderMustacheFile(
+  path: string,
+  data: Record<string, unknown>,
+): Promise<string> {
+  return mustache.render(decoder.decode(await Deno.readFile(path)), data);
+}
+
+const TEMPLATE_ENGINE = new Map<
+  SupportedTemplateEngine,
+  (path: string, data: Record<string, unknown>) => Promise<string>
+>([
+  [SupportedTemplateEngine.Mustache, renderMustacheFile],
+]);
+
+export { SupportedTemplateEngine, TEMPLATE_ENGINE };
