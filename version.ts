997914ff4862ce413481a886db15483ef70791c2
your-local-developer <73035294+your-local-developer@users.noreply.github.com>
2022-01-04 20:35:23 +0100

    Create version.ts
    Add support for publish

---
+++ version.ts
@@ -0,0 +1,21 @@
+/** `version` managed by https://deno.land/x/land/publish. */
+export const VERSION = "v0.0.0";
+
+/** `prepublish` will be invoked before publish, return `false` to prevent the publish. */
+export async function prepublish(version: string) {
+  const readme = await Deno.readTextFile("./README.md");
+
+  await Deno.writeTextFile(
+    "./README.md",
+    readme.replace(
+      /\/\/deno\.land\/x\/inertia_oak_middleware@v[\d\.]+\//,
+      `//deno.land/x/inertia_oak_middleware@${version}/`,
+    ),
+  );
+  return false;
+}
+
+/** `postpublish` will be invoked after published. */
+export async function postpublish(version: string) {
+  await console.log("Upgraded to", version);
+}
