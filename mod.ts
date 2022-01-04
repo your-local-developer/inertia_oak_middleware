825137043f7dc7c6476f0ed738d04858953d11c3
your-local-developer <73035294+your-local-developer@users.noreply.github.com>
2022-01-04 13:52:22 +0100

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -1,1 +1,5 @@
 export { Inertia } from "./src/inertia.ts";
+export type { InertiaConfig, InertiaState } from "./src/inertia.ts";
+export {
+  SupportedTemplateEngine
+} from "./src/supported_template_engines.ts";
